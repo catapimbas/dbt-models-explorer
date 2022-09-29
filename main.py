1bdc51e2e0735bd9c05dda15ed2c4e6464edf2b9
Rodrigo Cristiano <rcristianofv@hotmail.com>
2022-09-29 20:39:03 -0300

    ✨ First code commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+from dataclasses import dataclass
+
+import yaml
+
+
+@dataclass
+class Column:
+    name: str
+    description: str
+    props: dict
+
+
+@dataclass
+class Table:
+    name: str
+    description: str
+    columns: list[Column]
+
+
+def load_from_yaml(dir_path: str):
+    for dirpath, _, filenames in os.walk(dir_path):
+        for filename in filter(lambda name: name.endswith('.yml'), filenames):
+            with open(os.path.join(dirpath, filename)) as f:
+                try:
+                    model_dict = yaml.safe_load(f)
+                except yaml.YAMLError as e:
+                    print(e)
+                    break
+            for model in model_dict['models']:
+                yield load_table(model)
+
+
+def load_table(model_dict: dict):
+    return Table(
+        model_dict['name'],
+        model_dict.get('description'),
+        [Column(
+            column['name'],
+            column.get('description'),
+            column.get('tests'))
+         for column in model_dict['columns']])
